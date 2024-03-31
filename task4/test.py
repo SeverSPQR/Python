8e77f3baeae5bb5ef8cfc7369addf3b8c24eab96
pavlov <sin5900@mail.ru>
2024-03-31 14:26:23 +0300

    Проверка работоспособности СКВ

--- task4/test.py
+++ task4/test.py
@@ -1,18 +1,22 @@
 import Task4
+
+
 def test_passing():
     assert (1, 2, 3) == (1, 2, 3)
 
 
-def test_Len():
-    myFirstJournal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
-    assert len(myFirstJournal._parse()["todos"]) == 6
+def test_len():
+    journal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
+    assert len(journal._parse()["todos"]) == 6
 
-def test_AddEntry():
-    myFirstJournal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
-    myFirstJournal.add_entry("comprare videogioco")
-    assert myFirstJournal._parse()["todos"][len(myFirstJournal._parse()["todos"]) - 1] == "comprare videogioco"
 
-def test_RemoveEntryTest():
-    myFirstJournal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
-    myFirstJournal.remove_entry(len(myFirstJournal._parse()["todos"]) - 1)
-    assert len(myFirstJournal._parse()["todos"]) == 6
+def test_addentry():
+    journal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
+    journal.add_entry("comprare videogioco")
+    assert journal._parse()["todos"][len(journal._parse()["todos"]) - 1] == "comprare videogioco"
+
+
+def test_removeentrytest():
+    journal = Task4.TodoJournal('./task4.json','cosa io viglio fare oggi')
+    journal.remove_entry(len(journal._parse()["todos"]) - 1)
+    assert len(journal._parse()["todos"]) == 6
