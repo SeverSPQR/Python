42de39be2b28aa0f02eabb41bd625584349624e7
pavlov <sin5900@mail.ru>
2024-05-05 15:12:27 +0300

    Добавлен тест на удаление задачи, добавлены 3 фикстуры

--- task4/tests/test_todo_journal.py
+++ task4/tests/test_todo_journal.py
@@ -6,6 +6,7 @@ import json
 import pytest
 from src.TodoJournal import TodoJournal
 
+
 @pytest.fixture
 def tmpdir():
     filename = '../task4.json'
@@ -15,6 +16,36 @@ def tmpdir():
                 }
         json.dump(data, file)
     return filename
+
+
+@pytest.fixture()
+def todo_journal_with_3_entries(tmpdir):
+    todo_path = tmpdir
+    with open(todo_path, "w", encoding="utf-8") as f:
+        json.dump(
+            {
+                "name": "test",
+                "todos": ["first object", "second object", "third object"]
+            },
+            f,
+            indent=4,
+            ensure_ascii=False, )
+    return todo_path
+
+
+@pytest.fixture()
+def todo_json_after_remove_second_entry():
+    return {
+            "name": "test",
+            "todos": ["first object", "third object"]
+        }
+
+
+
+@pytest.fixture()
+def todo_object_with_with_3_entries(todo_journal_with_3_entries):
+    return TodoJournal(todo_journal_with_3_entries)
+
 
 @pytest.fixture
 def json_prepare():
@@ -31,8 +62,6 @@ def json_prepare():
                 }
         json.dump(data, file)
     return filename
-
-
 
 
 def test_len(json_prepare):
@@ -102,3 +131,10 @@ def test_create_journal(tmpdir):
     TodoJournal.create(filename, "test")
 
     assert expected_todo == todo._parse()
+
+
+def test_remove_entry(todo_object_with_with_3_entries, todo_json_after_remove_second_entry):
+    expected_todo_json_after_remove_second_entry = todo_json_after_remove_second_entry
+    todo_object_with_with_3_entries.remove_entry(1)
+
+    assert expected_todo_json_after_remove_second_entry == todo_object_with_with_3_entries._parse()
