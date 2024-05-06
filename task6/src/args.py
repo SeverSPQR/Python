5aca483c7d805a2218bf7e705c9d89c3f289c6b2
pavlov <sin5900@mail.ru>
2024-05-06 19:23:18 +0300

    Добавлена реализация функции parse_args(args) из задания

--- task6/src/args.py
+++ task6/src/args.py
@@ -1,3 +1,9 @@
+import argparse
+
 
 def parse_args(args):
-    pass
+    parser = argparse.ArgumentParser()
+    composing = parser.add_argument_group("Writing new todos",
+                                          "чтобы добавить новую запись в туду лист необходимо выполнить Х")
+    composing.add_argument("text", metavar="", nargs="*")
+    return parser.parse_intermixed_args(args)
--- task6/src/main.py
+++ task6/src/main.py
@@ -4,11 +4,11 @@ from args import parse_args
 from todo import run
 def main():
     try:
-        # 1. Спарсить аргументы командной
+
         cli_args = sys.argv[1:]
-        # 2. обработать аргументы командной строки
+
         args = parse_args(cli_args)
-        # 2. вызвать соответствующие функции
+
         return run(args)
     except Exception as e:
         print(e)
